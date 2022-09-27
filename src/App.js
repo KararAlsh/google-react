99092e561bf629b5a7a96a15330e53bb93edfa83
KararAlsh <kararabdulsahib@hotmail.com>
2022-09-27 17:04:57 +0700

    corrected the indentions

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react'
 class App extends React.Component {
   render() {
     return (
-			<section class="wrap">
-				<a href="/"><img className="results-logo"  src="img/google.png" /></a>
-				<form method="post" action="/results">
-				<input className="search-searchbar" type="text" name="search"/>
-				<button className="search-button">Search</button>
-				</form>
-			</section>
-
-		)
+     <section class="wrap">
+      <a href="/">
+       <img className="results-logo"  src="img/google.png" />
+      </a>
+      <form method="post" action="/results">
+       <input className="search-searchbar" type="text" name="search"/>
+       <button className="search-button">Search</button>
+      </form>
+     </section>
+   )
   }
 }
 export default App
